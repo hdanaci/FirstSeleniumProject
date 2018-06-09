3531dd386f48f9e2fdcaa6b0e606f1454ff19cf9
CybertekSchool <mr.danaci@yahoo.com>
2018-06-09 16:20:03 -0500

    wood

---
+++ src/com/cbt/GoogleSearch.java
@@ -0,0 +1,14 @@
+package com.cbt;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GoogleSearch {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver",
+				"/Users/bravo/Documents/selenium dependencies/drivers/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.etsy.com");
+	}
+}
