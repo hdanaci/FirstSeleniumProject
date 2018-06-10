49ea1c33a7e5ce53269fd1e89f83298e4eabe6a4
hdanaci <mr.danaci@yahoo.com>
2018-06-10 15:47:33 -0500

    ekle

--- src/com/cbt/GoogleSearch.java
+++ src/com/cbt/GoogleSearch.java
@@ -5,10 +5,26 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class GoogleSearch {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+/*
+ * open chrome
+ * go to url www.google.com
+ * verify title
+ * 	expected : Google
+ */
 		System.setProperty("webdriver.chrome.driver",
 				"/Users/bravo/Documents/selenium dependencies/drivers/chromedriver");
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.etsy.com");
+		driver.get("https://www.google.com");
+		
+		String expected = "Google";
+		String actual = driver.getTitle();
+		
+		if(expected.equals(actual)) {
+			System.out.println("pass");
+		}else {
+			System.out.println("fail");
+			System.out.println("Expected:\t" + expected);
+			System.out.println("Actual:\t" + actual);
+		}
 	}
 }
